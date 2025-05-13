95a67365c473bc2b26014e0fdee3fe1f1eb2c35c
Andreas Fagerlund <mcshaffa@gmail.com>
2025-05-13 21:59:33 +0200

    added some styling

--- scripts/views/playlistView.js
+++ scripts/views/playlistView.js
@@ -1,19 +1,21 @@
 export const renderPlaylists = (playlists) => {
-    const container = document.getElementById('playlist-container');
-    container.innerHTML = '';
+  const container = document.getElementById('playlists'); // OBS! matcha HTML-ID
+  container.innerHTML = '';
 
-    playlists.forEach(pl => {
-        const div = document.createElement('div');
-        div.className = 'playlist';
-        div.innerHTML = `
-            <h2>${pl.name}</h2>
-            <p>${pl.description}</p>
-            <ul>
-            ${pl.songs
-              .map((song) => `<li>${song.title} - ${song.artist}</li>`)
-              .join('')}
+  playlists.forEach((pl) => {
+    const div = document.createElement('div');
+    div.className = 'playlist bg-gray-800 text-white p-4 rounded shadow';
+    div.innerHTML = `
+            <h2 class="text-xl font-bold text-teal-400">${pl.name}</h2>
+            <p class="italic text-gray-300">${pl.description || ''}</p>
+            <p class="text-sm text-teal-300 mb-2">Genre: ${pl.genre}</p>
+            <ul class="list-disc list-inside">
+              ${pl.songs
+                .map((song) => `<li>${song.title} - ${song.artist}</li>`)
+                .join('')}
             </ul>
         `;
-        container.appendChild(div);
-    });
-}
+    container.appendChild(div);
+    
+  });
+};
